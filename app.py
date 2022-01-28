3348a8faca52de2c9fe9a171cc6d6f76b8b4ca7b
JEREMY0428 <98515526+JEREMY0428@users.noreply.github.com>
2022-01-28 23:56:00 +0800

    Update app.py

--- app.py
+++ app.py
@@ -81,112 +81,6 @@ def randomGuess(a,b,target):
     else: # 500/2001 約= 25%
         return '停損點位設好了嗎!?設定好就勇敢'+a+'吧!'
 
-def randomEat():
-    r = random.randint(0,51)
-    #print(r)
-    if r == 0: 
-        return '吃便當～'
-    elif r == 1:
-        return '吃滷肉飯～'
-    elif r == 2:
-        return '吃自助餐～'
-    elif r == 3:
-        return '吃乾麵～'
-    elif r == 4:
-        return '吃牛肉麵～'
-    elif r == 5:
-        return '吃麥噹噹～'
-    elif r == 6:
-        return '吃肯德基～'
-    elif r == 7:
-        return '吃摩斯～'
-    elif r == 8:
-        return '吃漢堡王～'
-    elif r == 9:
-        return '吃21世紀～'
-    elif r == 10:
-        return '吃炒飯～'
-    elif r == 11:
-        return '吃義大利麵～'
-    elif r == 12:
-        return '吃涼麵～'
-    elif r == 13:
-        return '吃三明治～'
-    elif r == 14:
-        return '吃沙拉～'
-    elif r == 15:
-        return '吃PIZZA～'
-    elif r == 16:
-        return '吃咖喱飯～'
-    elif r == 17:
-        return '吃御飯糰～'
-    elif r == 18:
-        return '吃關東煮～'
-    elif r == 19:
-        return '吃烏龍麵～'
-    elif r == 20:
-        return '吃壽司～'
-    elif r == 21:
-        return '吃超商微波食品～'
-    elif r == 22:
-        return '吃麵包～'
-    elif r == 23:
-        return '吃泡麵～'
-    elif r == 24:
-        return '吃小火鍋～'
-    elif r == 25:
-        return '吃雞排～'
-    elif r == 26:
-        return '吃鹹酥雞～'
-    elif r == 27:
-        return '吃麵線～'
-    elif r == 28:
-        return '吃雞胸肉～'
-    elif r == 29:
-        return '吃肉圓～'
-    elif r == 30:
-        return '吃韓式飯捲～'
-    elif r == 31:
-        return '吃春捲～'
-    elif r == 32:
-        return '吃河粉～'
-    elif r == 33:
-        return '吃拉麵～'
-    elif r == 34:
-        return '吃陽春麵～'
-    elif r == 35:
-        return '吃焗烤料理～'
-    elif r == 36:
-        return '吃炸雞～'
-    elif r == 37:
-        return '吃優格～'
-    elif r == 38:
-        return '吃水煮青菜～'
-    elif r == 39:
-        return '吃熱炒～'
-    elif r == 40:
-        return '吃麻辣燙～'
-    elif r == 41:
-        return '吃米粉湯～'
-    elif r == 42:
-        return '吃八方雲集～'
-    elif r == 43:
-        return '吃四海遊龍～'
-    elif r == 44:
-        return '吃燒烤～'
-    elif r == 45:
-        return '吃魷魚羹～'
-    elif r == 46:
-        return '吃泰式料理～'
-    elif r == 47:
-        return '喝蛋白粉～'
-    elif r == 48:
-        return '吃水餃～'
-    elif r == 49:
-        return '吃永和豆漿～'
-    else:
-        return '吃土～～'
-
 def parsingStr(pStr):
     splitStrArray = pStr.split(' ')
     
@@ -206,25 +100,23 @@ def parsingStr(pStr):
         elif pStr == 'Help' or pStr == 'help' or pStr == '幫助':
             outStr = ''
             outStr += '====== 指令清單 =====\n'
-            outStr += '1) 空/多 價格 (折扣)\n'
-            outStr += '簡易計算進出場的賺賠\n折扣為選填，格式為0.28，預設2.8折\n'
+            outStr += 'A) 空/多 價格 (折扣)\n'
+            outStr += '簡易計算進出場的賺賠\n折扣為選填，格式為0.25，預設2.5折\n'
             outStr += 'ex: 多 120.5 / 空 115 / 空 50.5 0.28\n\n'
-            outStr += '2) 價格\n'
+            outStr += 'B) 價格\n'
             outStr += '簡易計算上下1/1.5/2%大概為多少\n'
             outStr += 'ex: 90 / 215.5\n\n'
-            outStr += '3) count/Count/結算 總成交金額 應收付金額 (折扣)\n'
+            outStr += 'C) count/Count/結算 總成交金額 應收付金額 (折扣)\n'
             outStr += '幫助月退的計算損益\n折扣為選填，格式為2.5，預設2.5折\n'
             outStr += 'ex: Count 123000 365 0.25 / 結算 323000 -1255\n\n'
-            outStr += '4) 猜運勢(?)\n'
+            outStr += 'D) 猜運勢(?)\n'
             outStr += '用亂數來給建議(?)切勿盲目跟單(?)\n'
             outStr += '指令重點為開頭為多or空，結尾為如何，不用空格\n'
             outStr += 'ex: 空3035如何 / 多台積電如何\n\n'
-            outStr += '5) 亞當\n'
-            outStr += '輸入高/低點跟中間K棒開收，簡單試算滿足區大約在哪\n'
-            outStr += '格式為三個數字中間空白隔開，第一個為高/低點\n'
-            outStr += '後面兩個數字為中間K棒的開/收盤價(順序不重要)\n'
-            outStr += 'ex: 23.5 30 30.8\n\n'
-            outStr += '6) 出場價格參考\n'
+            outStr += 'E) 今日三關價\n'
+            outStr += '輸入昨日高/低點，簡單試算今日三關價大約在哪\n'
+            outStr += 'ex: Q 110 107\n\n'
+            outStr += 'F) 出場價格參考\n'
             outStr += '輸入 k/K 加上開盤價格，提供出場方式的參考點位\n'
             outStr += 'ex: k 50.4 / K 121.5\n\n'
             outStr += '111/01/28 ver 1.0.0'
@@ -234,6 +126,22 @@ def parsingStr(pStr):
                 return(randomGuess('空','多',pStr[1:-2]))
             else:
                 return(randomGuess('多','空',pStr[1:-2]))
+        elif '吃' in pStr:
+            return randomEat()
+        elif '帥' in pStr:
+            r = random.randint(0,1)
+            if r == 0:
+                return '本本 本本 好帥'
+            else:
+                return '酷酷 酷酷 帥爆了～'
+
+            #return '\大榮/\大榮/\大榮/ 帥爆了～'
+        elif '美' in pStr:
+            r = random.randint(0,1)
+            if r == 0:
+                return '八寶八寶 耶波搭'
+            else:
+                return '八寶八寶 耶波搭'
         else:
             return 'Error'
             
@@ -247,7 +155,7 @@ def parsingStr(pStr):
                     outStr = ''
                     if splitStrArray[0] == '空':
                         for k in range(targetIndex-5,targetIndex+5):
-                            fee = (targetValue + valueList[k]) * 1.425 * 0.28
+                            fee = (targetValue + valueList[k]) * 1.425 * 0.25
                             fee = round(fee,2)
                             tax = targetValue * 1.5
                             tax = round(tax,2)
@@ -269,7 +177,6 @@ def parsingStr(pStr):
                     return 'Error'
             else:
                 return 'Error'
-             
         elif splitStrArray[0] == 'k' or splitStrArray[0] == 'K':
             if is_number(splitStrArray[1]):
                 #return 'test'
@@ -288,9 +195,6 @@ def parsingStr(pStr):
                 return 'Error'
         else:
             return 'Error'
-         
-         
-         
     elif len(splitStrArray) == 3:
         if splitStrArray[0] == 'count' or splitStrArray[0] == 'Count' or splitStrArray[0] == '結算':
             if is_number(splitStrArray[1]) and is_number(splitStrArray[2]):
@@ -359,24 +263,6 @@ def parsingStr(pStr):
                 return 'Error'
         else:
             return 'Error'
-         
-         
-          elif splitStrArray[0] == 'q' or splitStrArray[0] == 'Q':
-            if is_number(splitStrArray[1]):
-                #return 'test'
-                targetValue = float(splitStrArray[1])
-                targetValue2 = float(splitStrArray[2])
-                outStr = ''
-                outStr += '昨日高點為 ' + splitStrArray[1] + "\n"
-                outStr += '1) 上關價為: ' + str(round(float(targetValue-targetValue2)/2,2)) + "\n"
-                #return 'test'
-                return outStr
-            else:
-                #return 'test2'
-                return 'Error'
-        else:
-            return 'Error'
-         
     elif len(splitStrArray) == 4:
         if splitStrArray[0] == 'count' or splitStrArray[0] == 'Count' or splitStrArray[0] == '結算':
             if is_number(splitStrArray[1]) and is_number(splitStrArray[2]) and is_number(splitStrArray[3]):
@@ -431,5 +317,3 @@ import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
-
-
